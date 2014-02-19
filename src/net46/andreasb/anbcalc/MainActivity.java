e26f576a51f5d3119cb7a1662bd644ce4ebc52bb
ylnomaps <mapsylno@alice.de>
2014-02-19 21:41:49 +0100

    second commit, still very very unfinshed

--- src/net46/andreasb/anbcalc/MainActivity.java
+++ src/net46/andreasb/anbcalc/MainActivity.java
@@ -20,6 +20,36 @@ public class MainActivity extends Activity
         text1=(TextView)findViewById(R.id.display1);
         text1.setText("0");
     }
+    static boolean isempty=true;
+    public void number_click (View sender)
+    {
+    	Button bt = (Button) sender;
+    	if (text1.getText().length()>7)
+    		return;
+    	if (isempty)
+    	{
+    		text1.setText(bt.getText());
+    		isempty=false;
+    	}
+    	else
+    	{
+    		text1.append(bt.getText());
+    	}
+    }
+    public void zero(View sender) {
+    	
+    	Button bt2 =(Button) sender;
+    	if (text1.getText()!="0")
+    	
+    	{
+    	text1.setText("");
+    	}
+    }
+    	
+    	
+    	
+    	
+    }
  
    
-}
+
