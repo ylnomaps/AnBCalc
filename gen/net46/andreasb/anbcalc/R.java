f2b61b06a56e5a3810931bc59c3e68ebf17b61df
ylnomaps <mapsylno@alice.de>
2014-02-20 16:50:25 +0100

    third commit

--- gen/net46/andreasb/anbcalc/R.java
+++ gen/net46/andreasb/anbcalc/R.java
@@ -14,24 +14,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f050009;
-        public static final int Button02=0x7f05000a;
-        public static final int Button03=0x7f05000b;
-        public static final int Button04=0x7f05000c;
-        public static final int Button05=0x7f05000e;
-        public static final int Button06=0x7f05000f;
-        public static final int Button07=0x7f050010;
-        public static final int Button08=0x7f050011;
-        public static final int Button1=0x7f050004;
-        public static final int Button2=0x7f050005;
-        public static final int Button3=0x7f050006;
-        public static final int Button4=0x7f050007;
+        public static final int Button01=0x7f05000b;
+        public static final int Button02=0x7f05000c;
+        public static final int Button03=0x7f05000d;
+        public static final int Button04=0x7f05000e;
+        public static final int Button05=0x7f050010;
+        public static final int Button06=0x7f050011;
+        public static final int Button07=0x7f050012;
+        public static final int Button08=0x7f050013;
+        public static final int Button1=0x7f050006;
+        public static final int Button2=0x7f050007;
+        public static final int Button3=0x7f050008;
+        public static final int Button4=0x7f050009;
         public static final int display1=0x7f050000;
-        public static final int number1=0x7f050001;
-        public static final int number2=0x7f050002;
-        public static final int tableRow1=0x7f050003;
-        public static final int tableRow2=0x7f050008;
-        public static final int tableRow3=0x7f05000d;
+        public static final int display2=0x7f050002;
+        public static final int dividiert=0x7f050017;
+        public static final int ergebnis=0x7f050004;
+        public static final int ist=0x7f050003;
+        public static final int mal=0x7f050018;
+        public static final int minus=0x7f050016;
+        public static final int operator=0x7f050001;
+        public static final int plus=0x7f050015;
+        public static final int tableRow1=0x7f050005;
+        public static final int tableRow2=0x7f05000a;
+        public static final int tableRow3=0x7f05000f;
+        public static final int tableRow4=0x7f050014;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -39,7 +46,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int display1=0x7f040003;
+        public static final int display2=0x7f040004;
+        public static final int ergebnis=0x7f040007;
+        public static final int ist=0x7f040006;
         public static final int number1=0x7f040001;
         public static final int number2=0x7f040002;
+        public static final int operator=0x7f040005;
     }
 }
--- src/net46/andreasb/anbcalc/MainActivity.java
+++ src/net46/andreasb/anbcalc/MainActivity.java
@@ -6,50 +6,77 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-	
-	TextView text1;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        text1=(TextView)findViewById(R.id.display1);
-        text1.setText("0");
-    }
-    static boolean isempty=true;
-    public void number_click (View sender)
-    {
-    	Button bt = (Button) sender;
-    	if (text1.getText().length()>7)
-    		return;
-    	if (isempty)
-    	{
-    		text1.setText(bt.getText());
-    		isempty=false;
-    	}
-    	else
-    	{
-    		text1.append(bt.getText());
-    	}
-    }
-    public void zero(View sender) {
-    	
-    	Button bt2 =(Button) sender;
-    	if (text1.getText()!="0")
-    	
-    	{
-    	text1.setText("");
-    	}
-    }
-    	
-    	
-    	
-    	
-    }
- 
-   
+public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
 
+	TextView text1;
+	TextView text2;
+	TextView operator1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		text1 = (TextView) findViewById(R.id.display1);
+		text1.setText("0");
+		text2 = (TextView) findViewById(R.id.display2);
+		text2.setText("0");
+		operator1 = (TextView) findViewById(R.id.operator);
+		operator1.setText("+");
+
+	}
+
+	static boolean wechsel = true;
+	static boolean isempty = true;
+
+	public void number_click(View sender) {
+		Button bt = (Button) sender;
+		if (text1.isInEditMode()) {
+			if (text1.getText().length() > 7)
+				return;
+			if (isempty) {
+				text1.setText(bt.getText());
+				isempty = false;
+			} else {
+				text1.append(bt.getText());
+			}
+		}
+		else if (text2.isInEditMode()) {
+			if (text2.getText().length() > 7)
+				return;
+			if (isempty) {
+				text2.setText(bt.getText());
+				isempty = false;
+			} else {
+				text2.append(bt.getText());
+			}
+		}
+		else return;
+	}
+
+	public void zero(View sender) {
+
+		Button bt2 = (Button) sender;
+		if (text1.getText() != "0")
+
+		{
+			text1.setText("");
+		}
+	}
+
+	public void operator_click(View sender) {
+		Button bt = (Button) sender;
+
+		operator1.setText(bt.getText());
+	}
+
+	public String operator = "plus";
+
+	public String minus(View sender) {
+
+		Button bt2 = (Button) sender;
+		operator = "minus";
+		return operator;
+	}
+
+}
